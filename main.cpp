1be14f7322d51cf09e0ea9a49cfdfb6fcf2dd4b2
Eric Sebastian <beowulfblackman@hotmail.com>
2023-08-30 14:20:50 +0000

    std::vector

--- main.cpp
+++ main.cpp
@@ -5,20 +5,28 @@
 #include <vector>
 
 static int rank, num_proces, bloque;
-float A[8*8];
-float B[8];
+static std::vector<float> A(4*4);
+static std::vector<float> B(4);
 
 static void llenar(){
-    for(int i=0; i < sizeof(A); i++){
+    for(int i=0; i < A.size(); i++){
         A[i]=1;
     }
 
-    for(int i=0; i < sizeof(B); i++){
+    for(int i=0; i < B.size(); i++){
         B[i]=2;
     }
 }
 
+static std:vector<float> multiplicar(std::vector<float> a, std::vector<float> b){
+    std::vector<float> tmp(a.size());
 
+    for(int i=0; i < a.size(); i++){
+        tmp[i] = a[i]*b[i];
+    }
+
+    return tmp;
+}
 
 int main(int argc, char **argv) {
 
@@ -33,7 +41,7 @@ int main(int argc, char **argv) {
     MPI_Bcast(&B, sizeof(B), MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        bloque = sizeof(A) /num_proces;
+        bloque = sizeof(A)/num_proces;
 
         for (int rank = 1; rank < num_proces; rank++) {
             MPI_Send(&A[(rank-1) * bloque], bloque, MPI_FLOAT, rank, 0, MPI_COMM_WORLD);
@@ -51,6 +59,7 @@ int main(int argc, char **argv) {
         MPI_Recv(&tmp, bloque, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         float resp = tmp;
+
         MPI_Send(&resp, bloque, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
     }
 
